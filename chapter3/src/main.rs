0e7e70450f500bbb1713a4cd146fe6c62d93ee4a
Andreas Bigger <abigger87@gmail.com>
2021-03-19 18:28:44 -0600

    finished rust book chapter 3 :sparkles:

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -52,6 +52,60 @@ fn main() {
     // * Return functions
     println!("The value of x is: {}", five());
     println!("The value of x + 1 is: {}", plus_one(10));
+
+    // * Control Flow
+    if five() < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = 3;
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn plus_one(x: i32) -> i32 {
