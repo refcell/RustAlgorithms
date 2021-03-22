9e718183a7db91cbd250b174caef1e22387bf8b2
Andreas Bigger <abigger87@gmail.com>
2021-03-22 07:57:10 -0700

    stash cargo stuff

---
+++ chapter14/target/package/chapter14-0.1.0/target/debug/chapter14.d
@@ -0,0 +1,1 @@
+/Users/andreasbigger/Projects/rust/chapter14/target/package/chapter14-0.1.0/target/debug/chapter14: /Users/andreasbigger/Projects/rust/chapter14/target/package/chapter14-0.1.0/src/lib.rs /Users/andreasbigger/Projects/rust/chapter14/target/package/chapter14-0.1.0/src/main.rs
---
+++ chapter14/target/package/chapter14-0.1.0/target/debug/libchapter14.d
@@ -0,0 +1,1 @@
+/Users/andreasbigger/Projects/rust/chapter14/target/package/chapter14-0.1.0/target/debug/libchapter14.rlib: /Users/andreasbigger/Projects/rust/chapter14/target/package/chapter14-0.1.0/src/lib.rs
